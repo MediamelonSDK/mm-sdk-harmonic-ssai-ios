e59037f5a271aa1585f2a6796ea8ad1c1f24b2cb
Aman Goel <aashishkumar@Aashishs-MacBook-Pro.local>
2024-09-23 12:41:26 +0530

    mm-sdk-harmonic-ssai-ios

---
+++ MMGenericFramework.xcframework/ios-arm64/MMGenericFramework.framework/Headers/MMGenericFramework.h
@@ -0,0 +1,29 @@
+//
+//  MMGenericFramework.h
+//  MMGenericFramework
+//
+//  Created by Anurag Singh on 11/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MMGenericFramework.
+FOUNDATION_EXPORT double MMGenericFrameworkVersionNumber;
+
+//! Project version string for MMGenericFramework.
+FOUNDATION_EXPORT const unsigned char MMGenericFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMGenericFramework/PublicHeader.h>
+
+
+//SSAI HEADERS
+
+//#import "M3U8Parser.h"
+//#import "M3U8ExtXByteRange.h"
+//#import "M3U8LineReader.h"
+//#import "M3U8TagsAndAttributes.h"
+//SSAI HEADERS
+
+#import "objc_mmsmartstreaming.h"
+#import "MMSmartStreaming.h"
+#import "MMSmartStreamingSDK.h"
